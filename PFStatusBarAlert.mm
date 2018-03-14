375cb73e812226df932dce6cd4b245d49de001bc
Andreas Henriksson <andreas.karl.henriksson@gmail.com>
2018-03-14 11:48:37 +0100

    Support for ARC

--- PFStatusBarAlert.mm
+++ PFStatusBarAlert.mm
@@ -1,4 +1,3 @@
-
 // PFStatusBarAlert
 
 #import <Foundation/Foundation.h>
@@ -6,10 +5,9 @@
 
 #import "PFStatusBarAlert.h"
 
-static void statusbar_got_notification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo )
-{
+static void statusbar_got_notification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo ) {
     if (observer)
-        [(PFStatusBarAlert *)observer showOverlayForSeconds:5.5];
+        [(__bridge PFStatusBarAlert *)observer showOverlayForSeconds:5.5];
 }
 
 @implementation PFStatusBarAlert
@@ -18,114 +16,87 @@ static void statusbar_got_notification(CFNotificationCenterRef center, void *obs
 @synthesize target=_target, actionButton=_actionButton, backgroundColor=_backgroundColor, textColor=_textColor;
 @synthesize oldLevel=_oldLevel;
 
-- (id)initWithMessage:(NSString *)message notification:(NSString *)notification action:(SEL)action target:(id)target
-{
- self = [self init];
+- (id)initWithMessage:(NSString *)message notification:(NSString *)notification action:(SEL)action target:(id)target {
+    self = [self init];
 
- // checking for screen orientation change
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateStatusBarFrame) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+    // checking for screen orientation change
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateStatusBarFrame) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
 
-// checking for status bar change
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateStatusBarFrame) name:UIApplicationWillChangeStatusBarFrameNotification object:nil];
-
-
- self.message = message;
-
- if (notification)
-  _notification = [notification copy];
-
- self.action = action;
- self.target = target;
-
- if (self.notification)
- {
-  CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), self, (CFNotificationCallback)statusbar_got_notification,
-  (CFStringRef)self.notification, NULL, CFNotificationSuspensionBehaviorCoalesce);
- }
+    // checking for status bar change
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateStatusBarFrame) name:UIApplicationWillChangeStatusBarFrameNotification object:nil];
 
 
- self.applicationWindow = [UIApplication sharedApplication].keyWindow;
- self.oldLevel = self.applicationWindow.windowLevel;
+    self.message = message;
 
- self.actionButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20)];
- self.actionButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
- self.actionButton.alpha = 0.0f;
+    if (notification)
+        _notification = [notification copy];
 
- if (self.target && self.action)
-  [self.actionButton addTarget:self.target action:self.action forControlEvents:UIControlEventTouchUpInside];
+    self.action = action;
+    self.target = target;
+
+    if (self.notification) {
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)statusbar_got_notification,
+        (CFStringRef)self.notification, NULL, CFNotificationSuspensionBehaviorCoalesce);
+    }
 
 
-  self.backgroundColor = [UIColor colorWithHue:0.15 saturation:0.00 brightness:0.96 alpha:1.00];
-  self.textColor = [UIColor blackColor];
+    self.applicationWindow = [UIApplication sharedApplication].keyWindow;
+    self.oldLevel = self.applicationWindow.windowLevel;
 
- self.actionButton.backgroundColor = self.backgroundColor;
+    self.actionButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20)];
+    self.actionButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.actionButton.alpha = 0.0f;
 
- return self;
+    if (self.target && self.action)
+        [self.actionButton addTarget:self.target action:self.action forControlEvents:UIControlEventTouchUpInside];
+
+
+    self.backgroundColor = [UIColor colorWithHue:0.15 saturation:0.00 brightness:0.96 alpha:1.00];
+    self.textColor = [UIColor blackColor];
+
+    self.actionButton.backgroundColor = self.backgroundColor;
+
+    return self;
 }
 
-- (void)updateStatusBarFrame
-{
+- (void)updateStatusBarFrame {
     UIView *view = self.actionButton;
     view.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20);
 }
 
-- (void)showOverlayForSeconds:(float)seconds
-{
+- (void)showOverlayForSeconds:(float)seconds {
+    if (self.actionButton.isHidden)
+        self.actionButton.hidden = NO;
 
-  if (self.actionButton.isHidden)
-    self.actionButton.hidden = NO;
+    self.applicationWindow.windowLevel = UIWindowLevelStatusBar;
 
-  self.applicationWindow.windowLevel = UIWindowLevelStatusBar;
+    [self.actionButton setTitle:self.message forState:UIControlStateNormal];
+    [self.actionButton setTitleColor:self.textColor forState:UIControlStateNormal];
+    self.actionButton.backgroundColor  = self.backgroundColor;
+    self.actionButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
 
-  [self.actionButton setTitle:self.message forState:UIControlStateNormal];
-  [self.actionButton setTitleColor:self.textColor forState:UIControlStateNormal];
-  self.actionButton.backgroundColor  = self.backgroundColor;
-  self.actionButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
+    [self.applicationWindow addSubview:self.actionButton];
+    [self.applicationWindow bringSubviewToFront:self.actionButton];
 
-  [self.applicationWindow addSubview:self.actionButton];
-  [self.applicationWindow bringSubviewToFront:self.actionButton];
-
-  [UIView animateWithDuration:0.3f animations:^{
-
-    self.actionButton.alpha = 1.0f;
-
-  } completion:^(BOOL finished) {
-
-    [NSTimer scheduledTimerWithTimeInterval:seconds
-    target:self
-    selector:@selector(hideOverlay)
-    userInfo:nil
-    repeats:NO];
-
-  }];
+    [UIView animateWithDuration:0.3f animations:^{
+        self.actionButton.alpha = 1.0f;
+    } completion:^(BOOL finished) {
+        [NSTimer scheduledTimerWithTimeInterval:seconds
+        target:self
+        selector:@selector(hideOverlay)
+        userInfo:nil
+        repeats:NO];
+    }];
 }
 
-- (void)hideOverlay
-{
-  [UIView animateWithDuration:0.3f animations:^{
-
-    self.actionButton.alpha = 0.0f;
-    self.applicationWindow.windowLevel = self.oldLevel;
-  } completion:^(BOOL finished) {
-
-    [self.actionButton removeFromSuperview];
-    self.actionButton.hidden = YES;
-  }];
-}
-
-- (void)dealloc
-{
-  self.message = nil;
-  self.target = nil;
-
-  if (_notification)
-    [_notification release];
-
-  self.actionButton = nil;
-  self.backgroundColor = nil;
-  self.textColor = nil;
-
-  [super dealloc];
+- (void)hideOverlay {
+    [UIView animateWithDuration:0.3f animations:^{
+        self.actionButton.alpha = 0.0f;
+        self.applicationWindow.windowLevel = self.oldLevel;
+    } completion:^(BOOL finished) {
+        [self.actionButton removeFromSuperview];
+        self.actionButton.hidden = YES;
+    }];
 }
 
 @end
